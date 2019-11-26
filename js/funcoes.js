4bf7108e52bd996473e03e8f3f18c24665c3ac8f
HUMBERTO BRUNO CARDOSO DOS SANTOS <0781921017@itaqua.fatec>
2019-11-26 13:14:32 -0300

    versão final index

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,5 @@
+//Ambientação de cores - mudança
+
 function botaocap() {
        document.getElementById("bd").style.background = "#100CE0";
 	   document.getElementById("navs").style.background = "#FF0000";
@@ -59,3 +61,117 @@ $('#exampleModal').on('show.bs.modal', function (event) {
   modal.find('.modal-body input').val(recipient)
 })
 
+//validação de CPF
+//---------------------------------------------------------------------------------------verifica cpf
+	//Link para validar CPF: https://www.devmedia.com.br/validar-cpf-com-javascript/23916
+	function validacaocpf(receberCpf){
+		var cpf = receberCpf;
+		var Soma;
+		var Resto;
+		Soma = 0;
+		resultadoInvalido = "";	
+		
+		if (cpf == "00000000000" || 
+			cpf == "11111111111" || 
+			cpf == "22222222222" || 
+			cpf == "33333333333" || 
+			cpf == "44444444444" || 
+			cpf == "55555555555" || 
+			cpf == "66666666666" || 
+			cpf == "77777777777" || 
+			cpf == "88888888888" || 
+			cpf == "99999999999")//return false;
+		{
+			alert("O CPF é inválido, pois está preenchido com " + cpf);
+			resultadoInvalido = "inválido";
+			return false;
+		}
+		
+		for (i=1; i<=9; i++) 
+		{
+			Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+		
+		}
+		Resto = (Soma * 10) % 11;
+			
+		if ((Resto == 10) || (Resto == 11))  
+		{
+			Resto = 0;
+			return false;
+		}
+		
+		if (Resto != parseInt(cpf.substring(9, 10)) ) //return false;
+		{
+			alert("O número do CPF é inválido");
+			resultadoInvalido = "inválido";
+			return false;
+		}
+	   
+		Soma = 0;
+		for (i = 1; i <= 10; i++){
+		Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+		}
+		Resto = (Soma * 10) % 11;
+	   
+		if ((Resto == 10) || (Resto == 11))
+		{
+			Resto = 0;
+			return false;
+		}
+		
+		if (Resto != parseInt(cpf.substring(10, 11) ) ) //return false;
+		{
+			alert("O número do CPF é inválido");
+			resultadoInvalido = "inválido";
+			return false;
+		}
+	}
+	
+	//Validação de cep 
+
+	function limpa_formulário_cep() {
+    document.getElementById('srtCep').value = ("");
+    document.getElementById('srtEndereco').value = ("");
+    document.getElementById('srtBairro').value = ("");
+    document.getElementById('srtCidade').value = ("");
+    document.getElementById('srtEstado').value = ("");
+    document.getElementById("msgCep").innerHTML = "";
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        document.getElementById('srtEndereco').value = (conteudo.logradouro);
+        document.getElementById('srtBairro').value = (conteudo.bairro);
+        document.getElementById('srtCidade').value = (conteudo.localidade);
+        document.getElementById('srtEstado').value = (conteudo.uf);
+        document.getElementById("msgCep").innerHTML = "<font color='green'>CEP válido!!</font>";
+    } else {
+        limpa_formulário_cep();
+        document.getElementById("msgCep").innerHTML = "<font color='red'>CEP não encontrado!!</font>";
+        //alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+    var cep = valor.replace(/\D/g, '');
+    if (cep != "") {
+        var validacep = /^[0-9]{8}$/;
+        if (validacep.test(cep)) {
+            document.getElementById('srtEndereco').value = "...";
+            document.getElementById('srtBairro').value = "...";
+            document.getElementById('srtCidade').value = "...";
+            document.getElementById('srtEstado').value = "...";
+            //document.getElementById('ibge').value = "...";
+            var script = document.createElement('script');
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+            document.body.appendChild(script);
+        } else {
+            limpa_formulário_cep();
+            document.getElementById("msgCep").innerHTML = "<font color='red'>CEP Inválido!!</font>";
+            alert("Formato de CEP inválido.");
+        }
+    } else {
+        limpa_formulário_cep();
+    }
+}
+//Fim do Testador de CEP//
